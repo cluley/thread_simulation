6472409d2f97de671ebe64c74df0e580befb3e36
Cluley <oo43@yandex.ru>
2023-07-25 21:39:26 +0700

    v0.7

--- main.cpp
+++ main.cpp
@@ -3,45 +3,61 @@
 #include <chrono>
 #include <Windows.h>
 #include <iomanip>
-#include <stdlib.h>
-#include <exception>
+#include <vector>
+#include <random>
+#include <mutex>
+
+std::mutex m;
 
 using namespace std::chrono_literals;
 
 void foo(const int th_num) {
+	std::lock_guard<std::mutex> lg(m);
 	auto start = std::chrono::steady_clock::now();
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 
-#pragma warning(push)
-#pragma warning(disable:4244)
-	try {
-		std::srand(time(NULL));
-		int rand = std::rand();
-		if ((rand % 2) == 0) throw std::exception();
-	}
-	catch (...) {
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
-	}
-#pragma warning(pop)
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<int> dist(0, 5);
 
-	std::cout << "Номер потока: " << th_num <<
-		"\nИдентификатор потока: " << std::this_thread::get_id() <<
-		'\n' << std::setfill('#') << std::setw(th_num) << '#' << std::endl;
+	for(int i = 0; i < 55; i += dist(gen)){
+		std::cout << '\r' << th_num << '\t' << std::this_thread::get_id() << "\t[";
+		if (i > 50) i = 50;
+
+		const size_t size = 1'000'000;
+		std::vector<int> v1(size), v2(size);
+		std::generate(v1.begin(), v1.end(), [&]() {
+			return dist(gen);
+			});
+		std::generate(v2.begin(), v2.end(), [&]() {
+			return dist(gen);
+			});
+
+		std::vector<int> temp = v1;
+		v1 = v2;
+		v2 = temp;
+		
+		std::cout << std::setfill('#') << std::setw(i) << '#' << std::setfill(' ') << std::setw(52 - i) << "] " << i * 2 << '%';
+		std::cout.flush();
+		std::this_thread::sleep_for(200ms);
+		if (i == 50) break;
+	}
 
 	auto end = std::chrono::steady_clock::now();
-	auto delta = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-	std::cout << delta.count() << "μs" << std::endl;
-
-	std::this_thread::sleep_for(800ms);
-	std::system("cls");
+	auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	std::cout << '\t' << delta.count() << "ms" << std::endl;
 }
 
 int main() {
-	const int length = 10;
+	const int qty = 12;
+	std::vector<std::thread> thds;
 
-	for (int i = 0; i < length; ++i)
-	{
-		std::thread th(foo, i + 1);
+	std::cout << '#' << "\tid" << "\t\t" << "\tProgress Bar\t\t\t\t\t" << "Time" << std::endl;
+
+	for (int i = 0; i < qty; ++i) {
+		thds.push_back(std::thread(foo, i));
+	}
+
+	for (auto& th : thds) {
 		th.join();
 	}
 
